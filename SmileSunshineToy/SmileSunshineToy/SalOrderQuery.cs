3467b43b44196ea3cea766fff22f573c533ffd2b
morgan <lvlianxizi@gmail.com>
2025-06-05 23:54:25 +0800

    deletesalelogic

--- SmileSunshineToy/SmileSunshineToy/SalOrderQuery.cs
+++ SmileSunshineToy/SmileSunshineToy/SalOrderQuery.cs
@@ -18,23 +18,8 @@ namespace SmileSunshineToy
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
 
-        }
 
-        private void signOutToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (DialogResult.Yes == MessageBox.Show("Are you sure to sign out?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
-            {
-                this.Close();
-            }
-        }
-
-        private void toolStripContainer1_ContentPanel_Load(object sender, EventArgs e)
-        {
-
-        }
 
         private void SalOrderQuery_Load(object sender, EventArgs e)
         {
@@ -68,49 +53,6 @@ namespace SmileSunshineToy
 
 
 
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void delect_col_Click(object sender, EventArgs e)
-        {
-            string connectionString = "Server=localhost;Database=test;Uid=root;Pwd=;";
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                connection.Open();
-                // 1. 先创建事务对象
-                MySqlTransaction trans = connection.BeginTransaction();
-                try
-                {
-                    MySqlCommand cmd = connection.CreateCommand();
-                    cmd.Transaction = trans; // 关联事务
-
-                    // 2. 添加具体的删除 SQL 语句，这里假设要删除 order 表数据，根据 OrderID 删除
-                    cmd.CommandText = "DELETE FROM `order` WHERE OrderID = @OrderID";
-                    foreach (DataGridViewRow row in dataGridView1.SelectedRows)
-                    {
-                        if (!row.IsNewRow) // 跳过新增行（如果有）
-                        {
-                            int orderId = Convert.ToInt32(row.Cells[1].Value); // 假设 OrderID 在第 2 列（索引 1）
-                            cmd.Parameters.Clear();
-                            cmd.Parameters.AddWithValue("@OrderID", orderId);
-                            cmd.ExecuteNonQuery();
-                            dataGridView1.Rows.Remove(row); // 从 DataGridView 中移除该行
-                        }
-                    }
-
-                    trans.Commit(); // 用事务对象提交
-                    MessageBox.Show("删除成功！");
-                }
-                catch (Exception ex)
-                {
-                    trans.Rollback(); // 失败回滚
-                    MessageBox.Show($"删除失败：{ex.Message}");
-                }
-            }
-        }
     }
-
 }
 
