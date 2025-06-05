745bb3c1d5ce5c1760dbcee1c729b5224d3b6475
morgan <lvlianxizi@gmail.com>
2025-06-06 00:12:55 +0800

    delte

--- SmileSunshineToy/SmileSunshineToy/RDdash.cs
+++ SmileSunshineToy/SmileSunshineToy/RDdash.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace SmileSunshineToy
 {
-    public partial class ProdPlanDetail : Form
+    public partial class RDdash : Form
     {
-        public ProdPlanDetail()
+        public RDdash()
         {
             InitializeComponent();
-        }
-
-        private void toolStripContainer1_ContentPanel_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
