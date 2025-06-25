708943cc8d54078f34171c7c666ed47df0fd9300
morgan <lvlianxizi@gmail.com>
2025-06-25 22:30:34 +0800

    uploas image

---
+++ SmileSunshineToy/SmileSunshineToy/ProductDetailForm.cs
@@ -0,0 +1,86 @@
+﻿using SmileSunshineToy.Utilities;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SmileSunshineToy
+{
+    public partial class ProductDetailForm : Form
+    {
+        private readonly int _productId;
+        private Image _currentImage;
+        private readonly FileUploadManager _fileUploadManager;
+        private readonly ProductImageHelper _imageHelper;
+
+        public ProductDetailForm(int productId, string productName, string description,
+                                 FileUploadManager fileUploadManager,
+                                 ProductImageHelper imageHelper)
+        {
+            InitializeComponent();
+            _productId = productId;
+            _fileUploadManager = fileUploadManager;
+            _imageHelper = imageHelper;
+
+            lblProductId.Text = $"产品ID: {productId}";
+            lblProductName.Text = productName;
+            txtDescription.Text = description;
+
+            LoadProductImage();
+        }
+
+        private void LoadProductImage()
+        {
+            // 获取当前图片
+            _currentImage = _imageHelper.GetProductImage(_productId);
+
+            if (_currentImage != null)
+            {
+                pictureBoxProduct.Image = _currentImage;
+            }
+            else
+            {
+                pictureBoxProduct.Image = Properties.Resources.DefaultProductImage;
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            // 选择并上传新图片
+            var newImage = _fileUploadManager.SelectAndUploadImage(_productId);
+
+            if (newImage != null)
+            {
+                // 显示新图片并释放旧资源
+                if (_currentImage != null && _currentImage != Properties.Resources.DefaultProductImage)
+                {
+                    _currentImage.Dispose();
+                }
+
+                _currentImage = newImage;
+                pictureBoxProduct.Image = _currentImage;
+
+                MessageBox.Show("图片上传成功!", "成功",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void ProductDetailForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 释放图片资源
+            if (_currentImage != null && _currentImage != Properties.Resources.DefaultProductImage)
+            {
+                _currentImage.Dispose();
+            }
+        }
+
+        private void ProductDetailForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SmileSunshineToy/SmileSunshineToy/ProductImageManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+public class ProductImageManager
+{
+    private readonly string _connectionString;
+
+    public ProductImageManager(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    // 保存图片到数据库
+    public void SaveImageToDb(int productId, Image image)
+    {
+        using (var conn = new MySqlConnection(_connectionString))
+        {
+            conn.Open();
+
+            using (var cmd = new MySqlCommand(
+                "UPDATE product SET image_data = @data, image_type = @type WHERE ProductID = @id",
+                conn))
+            {
+                cmd.Parameters.AddWithValue("@id", productId);
+                cmd.Parameters.AddWithValue("@data", ImageToBytes(image));
+                cmd.Parameters.AddWithValue("@type", "image/jpeg");
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    // 从数据库读取图片
+    public Image GetImageFromDb(int productId)
+    {
+        using (var conn = new MySqlConnection(_connectionString))
+        {
+            conn.Open();
+
+            using (var cmd = new MySqlCommand(
+                "SELECT image_data FROM product WHERE ProductID = @id",
+                conn))
+            {
+                cmd.Parameters.AddWithValue("@id", productId);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read() && !reader.IsDBNull(0))
+                    {
+                        byte[] data = (byte[])reader["image_data"];
+                        return BytesToImage(data);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    // 辅助方法：图片转字节数组
+    private byte[] ImageToBytes(Image image)
+    {
+        using (var ms = new MemoryStream())
+        {
+            image.Save(ms, ImageFormat.Jpeg);
+            return ms.ToArray();
+        }
+    }
+
+    // 辅助方法：字节数组转图片
+    private Image BytesToImage(byte[] data)
+    {
+        using (var ms = new MemoryStream(data))
+        {
+            return Image.FromStream(ms);
+        }
+    }
+}
